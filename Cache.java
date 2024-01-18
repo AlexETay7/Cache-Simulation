3de201521b4d96ff4c5670c5df76883c1c6c3a91
Alex Taylor <alexetaylor7@gmail.com>
2024-01-18 11:35:53 -0700

    first cache class design done

--- Cache.java
+++ Cache.java
@@ -14,14 +14,30 @@ public class Cache<T> {
     }
 
     /**
-     * Returns true if the data object is found
-     * in the list, false otherwise
-     * 
+     * Returns true if the data object is found in the list, false otherwise.
+     * If it's a cache hit, move the hit object to the front of the cache (MRU).
+     * If it's a cache miss, add the data to the front of the cache.
+     *
      * @param dataItem - data we are looking for
      * @return a boolean value
      */
     public boolean getObject(T dataItem) {
-        return cacheList.contains(dataItem);
+        boolean isHit = cacheList.contains(dataItem);
+
+        if (isHit) {
+            // Move the hit object to the front of the cache (MRU)
+            cacheList.remove(dataItem);
+        }
+
+        // Add the data item to the front of the cache
+        cacheList.addFirst(dataItem);
+
+        // If the cache size exceeds the limit, remove the oldest item
+        if (cacheList.size() > maxSize) {
+            removeObject(dataItem);
+        }
+
+        return isHit;
     }
 
     /**
@@ -30,17 +46,22 @@ public class Cache<T> {
      * @param dataItem - data to be added to top of cache
      */
     public void addObject(T dataItem) {
-
+        getObject(dataItem);
     }
 
-    // Method to remove an object from the cache
+    /**
+     * Removes the object from the cache.
+     * 
+     * @param dataItem - data to be removed
+     */
     public void removeObject(T dataItem) {
-
+        if (!cacheList.isEmpty()) {
+            cacheList.removeLast();
+        }
     }
 
-    // Method to clear the entire cache
     public void clearCache() {
-
+        cacheList.clear();
     }
 
 }

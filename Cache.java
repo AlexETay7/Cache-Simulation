12562f068f60ef93dc3797c6c80f8ffa74ff6210
Alex Taylor <alexetaylor7@gmail.com>
2024-01-18 11:04:27 -0700

    removed main

--- Cache.java
+++ Cache.java
@@ -1,10 +1,13 @@
 import java.util.LinkedList;
 
+/**
+ * A cache class that simulates
+ * cache behavior.
+ */
 public class Cache<T> {
     private LinkedList<T> cacheList;
     private int maxSize;
 
-    // Constructor
     public Cache(int maxSize) {
         this.maxSize = maxSize;
         this.cacheList = new LinkedList<>();
@@ -21,33 +24,23 @@ public class Cache<T> {
         return cacheList.contains(dataItem);
     }
 
-    // Method to add an object to the cache
+    /**
+     * Inserts data into top of cache (linkedlist)
+     * 
+     * @param dataItem - data to be added to top of cache
+     */
     public void addObject(T dataItem) {
-        // Implement the logic for adding an object to the cache
-        // ...
 
     }
 
     // Method to remove an object from the cache
     public void removeObject(T dataItem) {
-        // Implement the logic for removing an object from the cache
-        // ...
 
     }
 
     // Method to clear the entire cache
     public void clearCache() {
-        // Implement the logic for clearing the cache
-        // ...
 
     }
 
-    // Additional methods can be added based on specific requirements
-
-    // Test program
-    public static void main(String[] args) {
-        // Implement a test program to validate the Cache class
-        // ...
-
-    }
 }

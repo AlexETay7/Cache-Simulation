79fa250c3a1a239e831c362ec8293b3f7f886a23
Alex Taylor <alexetaylor7@gmail.com>
2024-01-18 11:00:24 -0700

    getObject method complete

--- Cache.java
+++ Cache.java
@@ -1,3 +1,53 @@
-public class Cache {
+import java.util.LinkedList;
 
+public class Cache<T> {
+    private LinkedList<T> cacheList;
+    private int maxSize;
+
+    // Constructor
+    public Cache(int maxSize) {
+        this.maxSize = maxSize;
+        this.cacheList = new LinkedList<>();
+    }
+
+    /**
+     * Returns true if the data object is found
+     * in the list, false otherwise
+     * 
+     * @param dataItem - data we are looking for
+     * @return a boolean value
+     */
+    public boolean getObject(T dataItem) {
+        return cacheList.contains(dataItem);
+    }
+
+    // Method to add an object to the cache
+    public void addObject(T dataItem) {
+        // Implement the logic for adding an object to the cache
+        // ...
+
+    }
+
+    // Method to remove an object from the cache
+    public void removeObject(T dataItem) {
+        // Implement the logic for removing an object from the cache
+        // ...
+
+    }
+
+    // Method to clear the entire cache
+    public void clearCache() {
+        // Implement the logic for clearing the cache
+        // ...
+
+    }
+
+    // Additional methods can be added based on specific requirements
+
+    // Test program
+    public static void main(String[] args) {
+        // Implement a test program to validate the Cache class
+        // ...
+
+    }
 }
